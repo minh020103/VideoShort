03382e8f1005b443dda42f613a65ab0f18edb0fa
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2024-03-18 23:49:27 +0700

    update

--- app/src/main/java/com/example/videoshort/view/activity/SplashActivity.java
+++ app/src/main/java/com/example/videoshort/view/activity/SplashActivity.java
@@ -2,15 +2,26 @@ package com.example.videoshort.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.videoshort.R;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, UserActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0,0);
+            }
+        }, 3000);
     }
 }
