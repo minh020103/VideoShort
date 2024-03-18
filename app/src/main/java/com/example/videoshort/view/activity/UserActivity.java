71ef4e8bedaa0782c430fe736d0f6f8bfc088cfc
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2024-03-18 23:49:12 +0700

    update

---
+++ app/src/main/java/com/example/videoshort/view/activity/UserActivity.java
@@ -0,0 +1,16 @@
+package com.example.videoshort.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.videoshort.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_layout);
+    }
+}
