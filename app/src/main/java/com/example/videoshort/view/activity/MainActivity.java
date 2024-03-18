9b5c8464a7bf9fe8a5ae75b5d714c307a4f3d8f6
Minh03-code <21211tt4504@mail.tdc.edu.vn>
2024-03-18 20:21:36 +0700

    setup

--- app/src/main/java/com/example/videoshort/view/activity/MainActivity.java
+++ app/src/main/java/com/example/videoshort/view/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.videoshort;
+package com.example.videoshort.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.videoshort.R;
 
 public class MainActivity extends AppCompatActivity {
 
